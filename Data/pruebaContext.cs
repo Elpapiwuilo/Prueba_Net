669f86513a0ce9f5ad555626f030aacd9aad0d1a
Sebastian <sebasipi501@gmail.com>
2019-06-21 09:40:35 -0500

    Initial commit

---
+++ Data/pruebaContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using prueba.Models;
+
+    public class pruebaContext : DbContext
+    {
+        public pruebaContext (DbContextOptions<pruebaContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<prueba.Models.Categoria> Categoria { get; set; }
+    }
---
+++ Models/Categoria.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prueba.Models
+{
+    public class Categoria
+    {
+        public int CategoriaID { get; set; }
+        [Required (ErrorMessage="Este campo es obligatorio")]
+        [StringLength(50,MinimumLength=3, ErrorMessage ="El Nombre debe de tener" + "3 a 50 caracteres")]
+        public string Nombre { get; set; }
+        [StringLength (256)]
+        public string Descripcion { get; set; }
+        public Boolean Estado { get; set; } = true;
+    }
+}
